a32d6387428edb3b341dfe47b256c3fa02fb30bd
AleksNest <99846088+AleksNest@users.noreply.github.com>
2023-04-19 11:52:21 +0300

    1

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ salaries = [15000, 20000, 25000]
 awards = ['10.0%', '7.25%', '5%']
 print(f'исходные данные:\n{names}\n{salaries}\n{awards}')
 print('Решение:')
+
 print({name: salary * float(award[:-1]) / 100 for name, salary, award in zip(names, salaries, awards)})
 
 '''
